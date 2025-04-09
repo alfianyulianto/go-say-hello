69f96b85b1b31fe9059e7468b3415f88c312a243
Alfian Yulianto <alfianyulianto36@gmail.com>
2025-04-10 05:38:34 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
-module github.com/alfianyulianto/go-say-hello
+// rubah nama module dengan menambahkan /v2 ketika ada major changes
+module github.com/alfianyulianto/go-say-hello/v2
 
 go 1.22.7
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,11 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World"
+//func SayHello() string {
+//	return "Hello World"
+//}
+
+// semua yang memanggil function SayHello akan rusak karena yang seblumnya tidak ada param menjadi ada param
+// inikah yang disebut major changes
+func SayHello(name string) string {
+	return "Hello " + name
 }
